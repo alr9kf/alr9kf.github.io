08d70a5d9ca713c370dc6cd50c710d233f616209
alr9kf <136037709+alr9kf@users.noreply.github.com>
2023-07-24 02:47:42 -0500

    Add files via upload

--- islt_7310/contact_process.php
+++ islt_7310/contact_process.php
@@ -1,9 +1,4 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'vendor/autoload.php';
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // collect value of input field
     $first_name = filter_var($_POST['first_name'], FILTER_SANITIZE_STRING);
@@ -22,33 +17,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = "New Contact Form Submission: ".$subject;
     $body = "You have received a new message from your contact form.\n\n"."Here are the details:\n\nName: ".$first_name." ".$last_name."\n\nEmail: ".$email."\n\nMessage:\n".$message;
 
-    $mail = new PHPMailer(true);
-
-    try {
-        //Server settings
-        $mail->SMTPDebug = 0;                                 
-        $mail->isSMTP();                                      
-        $mail->Host = 'smtp1.example.com';                    
-        $mail->SMTPAuth = true;                               
-        $mail->Username = 'user@example.com';                 
-        $mail->Password = 'secret';                           
-        $mail->SMTPSecure = 'tls';                            
-        $mail->Port = 587;                                    
-
-        //Recipients
-        $mail->setFrom('from@example.com', 'Mailer');
-        $mail->addAddress($to, 'Joe User');     
-
-        //Content
-        $mail->isHTML(true);                                  
-        $mail->Subject = $subject;
-        $mail->Body    = $body;
-
-        $mail->send();
-        echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo 'Message could not be sent.';
-        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    // send email
+    if(mail($to, $subject, $body)){
+        echo "Message Sent!";
+    } else {
+        echo "Failed to Send!";
     }
 }
 ?>
